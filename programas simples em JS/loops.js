d9cddcb283100994f2c0f5611a8a11dc79ffafdd
Antonio-Jefferson <98289047+Antonio-Jefferson@users.noreply.github.com>
2022-07-06 00:29:33 -0300

    versão 3.0

---
+++ programas simples em JS/loops.js
@@ -0,0 +1,23 @@
+
+//Estrutura de Repetição usando o "WHILE".
+
+var cont = 0;
+while (cont <= 10){
+    console.log (cont);
+    cont++;
+}
+ 
+//Estrutura de repetição Do WHILE.
+
+var c = 0;
+do{
+    console.log(c);
+    c++
+
+}while (c <= 10);
+
+// Estrutura de Repetição usando 'FOR'.
+
+for(var con = 0;con <= 10; con++){
+    console.log(con);
+}
